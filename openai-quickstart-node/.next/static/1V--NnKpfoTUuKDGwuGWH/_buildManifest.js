e3a8c268c6fc218fd1a9ed08c032ab2c252581d0
Paridhi <paridhigarg27@gmail.com>
2023-04-28 14:59:38 -0400

    build status

--- openai-quickstart-node/.next/static/1V--NnKpfoTUuKDGwuGWH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/602-c9e2d9a284ee12ca.js","static/chunks/pages/index-4518a9ac1e80101d.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
