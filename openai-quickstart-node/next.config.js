2a7749ee1ddbb2442887197af0ebc95b29fcf1b1
Paridhi <paridhigarg27@gmail.com>
2023-04-28 14:54:21 -0400

    asset prefix

--- openai-quickstart-node/next.config.js
+++ openai-quickstart-node/next.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/Wishful-Thinking' : '/openai-quickstart-node/Build/',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Wishful-Thinking/openai-quickstart-node/Build/' : '/',
   };
